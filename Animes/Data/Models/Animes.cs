0c8f90f824c13810e3787b82a723a795d7342b0f
assi23 <guiassi@hotmail.com>
2021-09-06 14:12:11 -0300

    creating models data

---
+++ Animes/Data/Models/Animes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+	public class Animes : Base
+	{
+		public string Name { get; set; }
+		public int Season { get; set; }
+		public int Episodes { get; set; }
+	}
+}
