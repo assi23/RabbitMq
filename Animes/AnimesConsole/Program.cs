c803f5f9d3b2548ed578af4b28bbcda24d19f014
assi23 <guiassi@hotmail.com>
2021-09-06 15:15:56 -0300

    Finishin the project, with console,Api and Data

---
+++ Animes/AnimesConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿using Data.Models;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+
+namespace AnimesConsole
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            var factory = new ConnectionFactory() { HostName = "192.168.0.140" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var anime = System.Text.Json.JsonSerializer.Deserialize<Animes>(body);
+                    Console.WriteLine(" [x] Received {0}", anime.Name);
+                };
+                channel.BasicConsume(queue: "product_queue",
+                                                            autoAck: true,
+                                                            consumer: consumer);
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ Animes/Api/Controllers/BaseController.cs
@@ -0,0 +1,41 @@
+﻿using Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController<M> : Controller where M : Base
+    {
+        [HttpPost]
+        public void Post(M model)
+        {
+            var factory = new ConnectionFactory() { HostName = "192.168.0.140" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                        queue: "product_queue",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null
+                        );
+
+                string message = System.Text.Json.JsonSerializer.Serialize(model);
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "",
+                                                routingKey: "product_queue",
+                                                basicProperties: null,
+                                                body: body);
+            }
+
+        }
+    }
+}
