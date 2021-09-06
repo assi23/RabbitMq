382c452eb270ad70489b978143e2d7af1e8104dc
assi23 <guiassi@hotmail.com>
2021-09-06 14:15:44 -0300

    Creating context data animes

---
+++ Animes/Data/Context/AnimesContext.cs
@@ -0,0 +1,19 @@
+﻿using Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Context
+{
+	public class AnimesContext : DbContext
+	{
+		public DbSet<Animes> Animes{ get; set; }
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseNpgsql("Host=localhost;Database=postgres;Username=postgres;Persist Security Info=True;Password=olist123");
+		}
+	}
+}
---
+++ Animes/Data/Migrations/20210906171456_CreateTableAnimes.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Data.Migrations
+{
+    public partial class CreateTableAnimes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Animes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Season = table.Column<int>(type: "integer", nullable: false),
+                    Episodes = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Animes");
+        }
+    }
+}
